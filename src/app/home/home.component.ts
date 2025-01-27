77c2dcbd5c1514d73e0af015cf75683491067af0
moe-jamal <moe.jamal.360@gmail.com>
2025-01-28 00:49:05 +0200

    slider fix

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,30 @@
 import {
   Component,
   CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
+import { SwiperContainer } from 'swiper/element';
 import { CustombtnComponent } from '../custombtn/custombtn.component';
+import { SlashsvgComponent } from '../svg/slashsvg/slashsvg.component';
 @Component({
   selector: 'app-home',
-  imports: [CustombtnComponent],
+  imports: [CustombtnComponent, SlashsvgComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent {
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef<SwiperContainer>;
+
   btnVariant: string = 'btn-trans';
+
+  slidePrev(): void {
+    this.swiperContainer.nativeElement.swiper.slidePrev();
+  }
+
+  slideNext(): void {
+    this.swiperContainer.nativeElement.swiper.slideNext();
+  }
 }
